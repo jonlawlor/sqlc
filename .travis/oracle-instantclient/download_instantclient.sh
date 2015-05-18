82c64f6844e27197904f01cb78e8c789b230ea45
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2015-05-17 20:53:05 -0400

    change to edelivery

--- .travis/oracle-instantclient/download_instantclient.sh
+++ .travis/oracle-instantclient/download_instantclient.sh
@@ -1,2 +1,13 @@
 #!/bin/sh -e
-wget -v -c --tries=2 -O $ORACLE_INSTANTCLIENT_FILE --user=$ORACLE_LOGIN_ssousername --password=$ORACLE_LOGIN_password "http://download.oracle.com/otn/linux/instantclient/121020/$ORACLE_INSTANTCLIENT_FILE"
+
+export COOKIES='cookies.txt'
+export USER_AGENT='Mozilla/5.0'
+
+echo > "$COOKIES"
+chmod 600 "$COOKIES"
+
+phantomjs --ssl-protocol=tlsv1 .travis/oracle/download_instantclient.js | head -n 1 |
+curl --cookie "$COOKIES" --cookie-jar "$COOKIES" --data '@-' \
+  --location --output "$ORACLE_INSTANTCLIENT_FILE" --user-agent "$USER_AGENT" \
+  'https://login.oracle.com/oam/server/sso/auth_cred_submit'
+
