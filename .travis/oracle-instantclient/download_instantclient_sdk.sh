54c3cd2f6cb9649798e449c0f376e155b009a8a8
Jonathan J Lawlor <jonathan.lawlor@gmail.com>
2015-05-17 21:04:26 -0400

    point phantomjs to the right files

--- .travis/oracle-instantclient/download_instantclient_sdk.sh
+++ .travis/oracle-instantclient/download_instantclient_sdk.sh
@@ -6,7 +6,7 @@ export USER_AGENT='Mozilla/5.0'
 echo > "$COOKIES"
 chmod 600 "$COOKIES"
 
-phantomjs --ssl-protocol=tlsv1 .travis/oracle/download_instantclient_sdk.js | head -n 1 |
+phantomjs --ssl-protocol=tlsv1 .travis/oracle-instantclient/download_instantclient_sdk.js | head -n 1 |
 curl --cookie "$COOKIES" --cookie-jar "$COOKIES" --data '@-' \
   --location --output "$ORACLE_INSTANTCLIENT_SDK_FILE" --user-agent "$USER_AGENT" \
   'https://login.oracle.com/oam/server/sso/auth_cred_submit'
